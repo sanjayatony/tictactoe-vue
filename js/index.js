450c2fcd9ec99410e5a11158658cad6642b93021
Tony S <sanjayatony@gmail.com>
2021-03-09 02:21:20 +0700

    make square clickable

--- js/index.js
+++ js/index.js
@@ -2,5 +2,22 @@ var app = new Vue({
   el: "#app",
   data: {
     squares: Array(9).fill(null),
+    xIsNext: true,
+  },
+  computed: {
+    player() {
+      return this.xIsNext ? "X" : "O";
+    },
+  },
+  methods: {
+    makeMove(index) {
+      this.squares.splice(index, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    },
+    newGame() {
+      this.squares = Array(9).fill(null);
+      this.xIsNext = true;
+      this.winner = null;
+    },
   },
 });
